9686689ff52c1912c28f62843c56e0c9721bec20
mm-dantas <matheusgithub@outlook.com>
2024-08-26 19:42:42 -0300

    adicionando header

---
+++ src/Componentes/Header/index.jsx
@@ -0,0 +1,16 @@
+import "./Header.css"
+
+function Header() {
+    return(
+        <header>
+            <nav>
+                <Link href="">Home</Link>
+                <Link href="">Sobre</Link>
+                <Link href="">Contato</Link>
+            </nav>
+        </header>
+    )
+    
+}
+
+export default Header
