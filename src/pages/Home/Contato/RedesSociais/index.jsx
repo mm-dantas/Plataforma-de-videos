d56fc70e86a677501d49309456ee7f3c9d0a9e15
Marco163b <marcotostines@hotmail.com>
2024-09-03 19:30:14 -0300

    Adicionando Imagens

--- src/pages/Home/Contato/RedesSociais/index.jsx
+++ src/pages/Home/Contato/RedesSociais/index.jsx
@@ -1,6 +1,6 @@
-import Discord from '../../../../assets/Discord.png'
-import Twitter from '../../../../assets/Twitter.png'
-import Instagram from '../../../../assets/Instagram.png'
+import Discord from "../../../../assets/Discord.png";
+import Twitter from "../../../../assets/Twitter.png";
+import Instagram from "../../../../assets/Instagram.png";
 
 
 
@@ -8,11 +8,9 @@ function RedesSociais(){
     return(
         <>
             <h1>Dúvidas e suporte, entre em contato:</h1>
-            {/* <img src="{Twitter}" alt="Logo do Twitter" />
-            <img src="{Discord}" alt="Logo do Discord" />
-            <img src="{Instagram}" alt="Logo do Instagram" /> */}
-            <img src="disc.jpg" alt="aham" />
-
+            <img src={Twitter} alt="Logo do Twitter" />
+            <img src={Discord} alt="Logo do Discord" />
+            <img src={Instagram} alt="Logo do Instagram" />
         </>
     )
 }
