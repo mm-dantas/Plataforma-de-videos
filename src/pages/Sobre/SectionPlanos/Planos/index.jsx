480c62008ad36cee55238bbccd596eccf8ff1129
Marco163b <marcotostines@hotmail.com>
2024-09-06 17:24:02 -0300

    mudanças pagina sobre

--- src/pages/Sobre/SectionPlanos/Planos/index.jsx
+++ src/pages/Sobre/SectionPlanos/Planos/index.jsx
@@ -1,12 +1,41 @@
-import './Planos.css'
+import React from 'react';
+import './Planos.css'; 
+// import Coorp from '../../assets/FtCoorp.png';
 
+const Planos = () => {
+  return (
+    <div className="planos-container">
+      <h2>Planos</h2>
+      <div className="cards-container">
+        <div className="card">
+          <h3>Individual</h3>
+          <p>1 Usuário</p>
+          <select>
+            <option>10 Vídeos R$15</option>
+          </select>
+          <button>Cadastrar</button>
+        </div>
 
-function Planos(){
-    return(
-        <>
-            <img src="" alt="" />
-        </>
-    )
-}
+        <div className="card">
+          <h3>Profissional - Times</h3>
+          <p>1-10 Usuários</p>
+          <select>
+            <option>Vídeos Ilimitados R$40</option>
+            <option>+10 Usuários R$20</option>
+          </select>
+          <button>Cadastrar</button>
+        </div>
+
+        <div className="card corporativo">
+          <h3>Corporativo</h3>
+          <div className="image-placeholder">
+            {/* <img src={Coorp} alt="Corporativo" /> */}
+          </div>
+          <button>Entre Em Contato</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Planos;
